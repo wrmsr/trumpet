5480990dfd54985a6e80b07f9d04d5bdd6b360b5
Will Timoney <will@remind101.com>
2016-10-19 18:50:49 -0700

    --

--- trumpet_test.go
+++ trumpet_test.go
@@ -4,10 +4,19 @@ import (
 	"database/sql"
 	"fmt"
 	"testing"
+	_ "./postgres"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func TestRun(t *testing.T) {
 	Run()
+}
+
+func chkerr(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestInformationSchema(t *testing.T) {
@@ -44,4 +53,36 @@ func TestInformationSchema(t *testing.T) {
 		}
 		fmt.Println()
 	}
+}
+
+
+type Product struct {
+	gorm.Model
+	Code string
+	Price uint
+}
+
+func TestGorm(t *testing.T) {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// Read
+	var product Product
+	db.First(&product, 1) // find product with id 1
+	db.First(&product, "code = ?", "L1212") // find product with code l1212
+
+	// Update - update product's price to 2000
+	db.Model(&product).Update("Price", 2000)
+
+	// Delete - delete product
+	db.Delete(&product)
 }
